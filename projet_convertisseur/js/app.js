98bf541ebcf0066bf5921a723453b18538bd81c9
Lux <lmoinel@edenschool.fr>
2022-11-29 22:24:07 +0100

    correction

--- projet_convertisseur/js/app.js
+++ projet_convertisseur/js/app.js
@@ -1,12 +1,10 @@
 let result = document.getElementById("result");
-let euros = document.getElementsByName("euro");
-let francs = document.getElementsByName("franc");
 let input = document.querySelectorAll("input");
 let convert = 6.55957;
 
 for (let i = 0; i < input.length; i++) {
   const element = input[i];
-  element.addEventListener("input", (e) => {
+  element.addEventListener("input", () => {
     if (element.name == "euro") {
       result.innerHTML = "Converti en franc = " + element.value * convert;
     } else if (element.name == "franc") {
@@ -14,5 +12,3 @@ for (let i = 0; i < input.length; i++) {
     }
   });
 }
-
-// 6.55957
